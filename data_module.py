933b19cd762bf4bb4b2e21c1ef62b7c49d7fe456
over <jeanlepeltier1@gmail.com>
2024-10-11 14:07:39 +0200

    ajout de load_user et update_user dans la classe Data

--- data_module.py
+++ data_module.py
@@ -1,11 +1,22 @@
 from fonctions_annexes import *
+import os
 
 class Data:
     
     def __init__():
         pass
 
-
-
-    def load_user(self,name):
+    def load_user(self,pseudo):
+        path_user = f"{pseudo}.json"
+        if os.path.exists(path_user):
+            return load_json_data(path_user)
+        else :
+            return None
+        
+    def update_user(self,new_data):
+        if dump_json_data(new_data) != 0:
+            print("Problème lors de la mise à jour des données du user")
+            return 1
+        else :
+            return 0
         
--- fonctions_annexes.py
+++ fonctions_annexes.py
@@ -1,7 +1,17 @@
 import json
 
-def dump_json(path_file):
-    with open(path_file, 'r') as file:
-        json_content = json.load(path_file)
+def load_json_data(path_file):
+    try :
+        with open(path_file, 'r') as file:
+            json_content = json.load(path_file)
+        return json_content
+    except :
+        return None
 
-    return json_content
+def dump_json_data(path_file, data):
+    try :
+        with open(path_file, 'w') as file:
+            json.dump(data, file)
+        return 0
+    except:
+        return 1
